4af0e0aea45cc0a0deca27133c5ccad6b00d4350
duszekjk <jacek@duszekjk.com>
2026-03-20 08:59:35 +0100

    2nd workflow try

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-async def root():
+def hello():
     return {"message": "Hello World"}
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/add/{a}/{b}")
+def add(a: int, b: int):
+    return {"result": a + b}
